03a1ef3cf71dd4676a39b6595a5e8a5351b0e48c
Akshay Dixit <akshaydixi@gmail.com>
2014-10-15 15:47:11 +0530

    changed build and python script according to ci config

--- build.sh
+++ build.sh
@@ -10,5 +10,6 @@ mvn clean package
 export BASE_DIR=$(cd `dirname $0`; pwd -P)
 export TARGET_LOCATION="${BASE_DIR}/wayback-webapp/target"
 cp "${TARGET_LOCATION}/dependency/jetty-runner.jar" $TARGET_LOCATION
+cp "${BASE_DIR}/wayback" $TARGET_LOCATION
 export WAR_LOCATION=`find_war ${TARGET_LOCATION} openwayback`
-tar -C ${TARGET_LOCATION} -cvzf "${TARGET_LOCATION}/wayback.tar.gz" "`basename $WAR_LOCATION`" "jetty-runner.jar"
+tar -C ${TARGET_LOCATION} -cvzf "${TARGET_LOCATION}/wayback.tar.gz" "`basename $WAR_LOCATION`" "jetty-runner.jar" "wayback"
