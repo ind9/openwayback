ac386a7a9467315660e3453da4ae1e01d0f1fe7b
Akshay Dixit <akshaydixi@gmail.com>
2014-11-12 17:53:09 +0530

    3 day data config in generate_cdx.sh

--- generate_cdx.sh
+++ generate_cdx.sh
@@ -4,8 +4,14 @@ DATETIME="`date --date="yesterday" +"%Y%m%d"`"
 CDX_HDFS_PATH_REGEX="${CDX_HDFS_PATH}${CDX_LOCATION}/${DATETIME}_cdx/*.cdx"
 WARC_TARGET_PATH="${WARC_HDFS_PATH}${WARC_LOCATION}/${DATETIME}_warc"
 WARC_TARGET_PATH_REGEX="${WARC_TARGET_PATH}/*.warc"
+NEWTMPFILE="/tmp/temp2.file"
 TEMPFILE="/tmp/temp.file"
 CDXTEMPFILE="/tmp/index.cdx"
+ONEDAYAGOCDX="`dirname $CDXFILE`/onedayago.cdx"
+TWODAYSAGOCDX="`dirname $CDXFILE`/twodaysago.cdx"
+THREEDAYSAGOCDX="`dirname $CDXFILE`/threedaysago.cdx"
+ONEDAYAGOPATHINDEX="`dirname $PATH_INDEX`/path-index1.txt"
+TWODAYSAGOPATHINDEX="`dirname $PATH_INDEX`/path-index2.txt"
 
 touch $CDXTEMPFILE
 echo " CDX N b a m s k r M S V g" >> $CDXTEMPFILE
@@ -23,6 +29,8 @@ cd $CDX_TARGET_PATH; ls | xargs -n 32 -P 8 cat >> $CDXTEMPFILE
 for filename in `hadoop dfs -ls $WARC_TARGET_PATH_REGEX | sed '1d;s/  */ /g' | cut -d\  -f8 | xargs -n 1 basename`; do
   echo "$filename $WARC_TARGET_PATH/$filename" >> $TEMPFILE
 done
+cp $TEMPFILE $NEWTMPFILE
+cat $ONEDAYAGOPATHINDEX $TWODAYSAGOPATHINDEX >> $TEMPFILE
 
 export LC_ALL=C;
 sort $TEMPFILE > $PATH_INDEX
@@ -31,3 +39,9 @@ sort $CDXTEMPFILE > $CDXFILE
 rm -rf $CDX_TARGET_PATH
 rm $TEMPFILE
 rm $CDXTEMPFILE
+rm $THREEDAYSAGOCDX
+mv $TWODAYSAGOCDX $THREEDAYSAGOCDX
+mv $ONEDAYAGOCDX $TWODAYSAGOCDX
+mv $CDXFILE $ONEDAYAGOCDX
+mv $ONEDAYAGOPATHINDEX $TWODAYSAGOPATHINDEX
+mv $NEWTMPFILE $ONEDAYAGOPATHINDEX
